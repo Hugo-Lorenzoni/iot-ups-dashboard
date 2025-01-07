3535d49b870120e458d341f0e43c3660dae04fd6
Hugo Lorenzoni <210937@umons.ac.be>
2025-01-07 04:39:05 +0100

    fix access key and secret synthax

--- src/lib/minio.ts
+++ src/lib/minio.ts
@@ -7,8 +7,8 @@ const minioClient = new Minio.Client({
   endPoint: process.env.MINIO_ENDPOINT || "localhost",
   port: Number(process.env.MINIO_PORT),
   useSSL: process.env.MINIO_USE_SSL === "true",
-  accessKey: process.env.MINIO_ACCESS_KEY || "minio",
-  secretKey: process.env.MINIO_SECRET_KEY || "secret",
+  accessKey: process.env.ACCESSKEY || "minio",
+  secretKey: process.env.SECRETKEY || "secret",
 });
 
 export default minioClient;
